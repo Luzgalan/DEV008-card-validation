380e13c023d7cba7a1a70e3227d4f44eb0e9a5aa
Luz Vazquez <vgluzmaria96@gmail.com>
2023-05-03 23:08:40 -0600

    validacion en validator y maskify

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import validator from "./validator.js";
 
 window.validarTarjeta = function () {
-  const numero = document.getElementById("iptTarjeta").value;
-  console.log("numero desde index", numero);
+  const numero = document.getElementById("iptTarjeta").value; //traer un valor al html
+
+  /* ------------------- validar cuando el campo esta vacio ------------------- */
+  if (numero === "") {
+    alert("No se puede enviar un campo vacio");
+    return;
+  }
   const esValido = validator.isValid(numero);
-  console.log(esValido);
+  const mascara = validator.maskify(numero);
+  let franquicia;
+  document.getElementById("iptTarjeta").value = mascara; //mandar un valor al htmls
+  if (esValido) {
+    franquicia = validator.obtenerFranquicia(numero);
+    alert("tarjeta valida");
+  } else {
+    alert("Tarjeta no valida");
+  }
+
+  document.getElementById("btnNuevo").style.display = "block"; //muestra el boton
+  document.getElementById("btnValidar").style.display = "none"; //oculte
+  document.getElementById("iptTarjeta").disabled = true; //habilita el input
+
+  console.log(franquicia);
+};
+
+window.validarNuevaTarjeta = function () {
+  document.getElementById("btnNuevo").style.display = "none";
+  document.getElementById("btnValidar").style.display = "block";
+  document.getElementById("iptTarjeta").value = "";
+  document.getElementById("iptTarjeta").disabled = false; //habilita el input
+  document.getElementById("iptTarjeta").focus(); //coloca el cursor en el input
 };
--- src/validator.js
+++ src/validator.js
@@ -1,24 +1,59 @@
 const validator = {
   isValid: function (creditCardNumber) {
-    /* ------------------- validar cuando el campo esta vacio ------------------- */
-    if (creditCardNumber === "") {
-      alert("Ingrese un numero");
-      return false;
-    }
-
     /* --------------------- convertir el string a un array --------------------- */
     const arrayTarjeta = creditCardNumber.split("");
-    console.log(arrayTarjeta);
 
     /* ---------------------- validar que tenga 16 digitos ---------------------- */
-    if (arrayTarjeta.length !== 16) {
-      alert("La tarjeta debe contener 16 digitos");
-      return false;
-    }
+    // if (arrayTarjeta.length !== 16) {
+    //   return false;
+    // }
 
     /* -------------------------------------------------------------------------- */
     /*                          iniciar algoritmo de luhn                         */
     /* -------------------------------------------------------------------------- */
+
+    arrayTarjeta.reverse();
+
+    for (let i = 1; i < arrayTarjeta.length; i = i + 2) {
+      arrayTarjeta[i] = arrayTarjeta[i] * 2;
+      if (arrayTarjeta[i] > 9) {
+        arrayTarjeta[i] = arrayTarjeta[i] - 9;
+      }
+    }
+
+    let suma = 0;
+    arrayTarjeta.forEach((numero) => {
+      suma = suma + parseInt(numero, 10);
+    });
+    return suma % 10 === 0 ? true : false;
+  },
+
+  maskify: function (creditCardNumber) {
+    const arrayTarjeta = creditCardNumber.split("");
+    for (let i = 0; i < arrayTarjeta.length - 4; i++) {
+      arrayTarjeta[i] = "#";
+    }
+    return arrayTarjeta.join("");
+  },
+
+  obtenerFranquicia: function (creditCardNumber) {
+    let inicioTarjeta = creditCardNumber.substring(0, 2);
+    if (
+      inicioTarjeta === "51" ||
+      inicioTarjeta === "52" ||
+      inicioTarjeta === "53" ||
+      inicioTarjeta === "54" ||
+      inicioTarjeta === "55"
+    ) {
+      return "Mastercard";
+    }
+    if (inicioTarjeta === "50") {
+      return "Carnet";
+    }
+    inicioTarjeta = creditCardNumber.substring(0, 1);
+    if (inicioTarjeta === 4) {
+      return "Visa";
+    }
   },
 };
 
